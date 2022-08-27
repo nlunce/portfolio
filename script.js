10bd5bd144571ff9d20baf766a2cec19acdb5d77
nlunce <naterrl15@byui.edu>
2022-08-27 16:11:08 -0600

    js and css changes

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-"strict mode";
+'strict mode';
+
+const navHighlight = function () {
+  document.querySelector('.home').style.color = 'var(--accent-color)';
+  document.querySelector('.fa-solid').style.color = 'var(--accent-color)';
+};
+
+if (document.querySelector('title').textContent === 'Home') {
+  navHighlight();
+}if else(document.querySelector('title').textContent === 'About'){
+    navHighlight();
+}if else(document.querySelector('title').textContent === 'Services'){
+    navHighlight();
+}if else(document.querySelector('title').textContent === 'Portfolio'){
+    navHighlight();
+}if else(document.querySelector('title').textContent === 'Contact'){
+    navHighlight();
+}
+
+
+
