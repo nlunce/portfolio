84f538a06a0ae919b607ead45d75bf9ff6559803
nlunce <naterrl15@byui.edu>
2022-08-28 07:24:55 -0600

    style js

--- script.js
+++ script.js
@@ -1,21 +1,29 @@
 'strict mode';
+const home = document.querySelector('.home').className;
+const about = document.querySelector('.about').className;
+const services = document.querySelector('.services').className;
+const portfolio = document.querySelector('.portfolio').className;
+const contact = document.querySelector('.contact').className;
 
-const navHighlight = function () {
-  document.querySelector('.home').style.color = 'var(--accent-color)';
-  document.querySelector('.fa-solid').style.color = 'var(--accent-color)';
+const homeIcon = document.querySelector('.fa-house').className;
+const aboutIcon = document.querySelector('.fa-user').className;
+const servicesIcon = document.querySelector('.fa-bars').className;
+const portfolioIcon = document.querySelector('.fa-briefcase').className;
+const contactIcon = document.querySelector('.fa-comments').className;
+
+const navHighlight = function (navTitle, navIcon) {
+  document.querySelector(`.${navTitle}`).style.color = 'var(--accent-color)';
+  document.querySelector(`.${navIcon}`).style.color = 'var(--accent-color)';
 };
 
 if (document.querySelector('title').textContent === 'Home') {
-  navHighlight();
-}if else(document.querySelector('title').textContent === 'About'){
-    navHighlight();
-}if else(document.querySelector('title').textContent === 'Services'){
-    navHighlight();
-}if else(document.querySelector('title').textContent === 'Portfolio'){
-    navHighlight();
-}if else(document.querySelector('title').textContent === 'Contact'){
-    navHighlight();
+  navHighlight(home, homeIcon);
+} else if (document.querySelector('title').textContent === 'About') {
+  navHighlight(about, aboutIcon);
+} else if (document.querySelector('title').textContent === 'Services') {
+  navHighlight(services, servicesIcon);
+} else if (document.querySelector('title').textContent === 'Portfolio') {
+  navHighlight(portfolio, portfolioIcon);
+} else if (document.querySelector('title').textContent === 'Contact') {
+  navHighlight(contact, contactIcon);
 }
-
-
-
